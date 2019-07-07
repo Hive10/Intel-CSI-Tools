338231325d37988242fca06707f68f2363771ad7
Alberto-czf <2856193308@qq.com>
2019-07-07 11:45:29 +0800

    csi-supplementary

---
+++ linux-80211n-csitool-supplementary/netlink/syn_log_to_file.c
@@ -0,0 +1,167 @@
+/*
+ * (c) 2019 Wang Zhixiang <2856193308@qq.com>
+ */
+#include "iwl_connector.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+
+#include<time.h>
+
+#define MAX_PAYLOAD 2048
+#define SLOW_MSG_CNT 1
+
+int sock_fd = -1;							// the socket
+FILE* out = NULL;
+
+void check_usage(int argc, char** argv);
+
+FILE* open_file(char* filename, char* spec);
+
+void caught_signal(int sig);
+
+void exit_program(int code);
+void exit_program_err(int code, char* func);
+
+int main(int argc, char** argv)
+{
+	/* Local variables */
+	struct sockaddr_nl proc_addr, kern_addr;	// addrs for recv, send, bind
+	struct cn_msg *cmsg;
+	char buf[4096];
+	int ret;
+	unsigned short l, l2, l3;
+	int count = 0;
+	unsigned long int timestamp;
+	char data[0];
+	
+	struct timespec now; //record the time of package received by user.
+
+	/* Make sure usage is correct */
+	check_usage(argc, argv);
+
+	/* Open and check log file */
+	out = open_file(argv[1], "w");
+
+	/* Setup the socket */
+	sock_fd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_CONNECTOR);
+	if (sock_fd == -1)
+		exit_program_err(-1, "socket");
+	
+
+	/* Initialize the address structs */
+	memset(&proc_addr, 0, sizeof(struct sockaddr_nl));
+	proc_addr.nl_family = AF_NETLINK;
+	proc_addr.nl_pid = getpid();			// this process' PID
+	proc_addr.nl_groups = CN_IDX_IWLAGN;
+	memset(&kern_addr, 0, sizeof(struct sockaddr_nl));
+	kern_addr.nl_family = AF_NETLINK;
+	kern_addr.nl_pid = 0;					// kernel
+	kern_addr.nl_groups = CN_IDX_IWLAGN;
+
+	/* Now bind the socket */
+	if (bind(sock_fd, (struct sockaddr *)&proc_addr, sizeof(struct sockaddr_nl)) == -1)
+		exit_program_err(-1, "bind");
+
+	/* And subscribe to netlink group */
+	{
+		int on = proc_addr.nl_groups;
+		ret = setsockopt(sock_fd, 270, NETLINK_ADD_MEMBERSHIP, &on, sizeof(on));
+		if (ret)
+			exit_program_err(-1, "setsockopt");
+	}
+
+	/* Set up the "caught_signal" function as this program's sig handler */
+	signal(SIGINT, caught_signal);
+
+	/* Poll socket forever waiting for a message */
+	while (1)
+	{
+		/* Receive from socket with infinite timeout */
+		ret = recv(sock_fd, buf, sizeof(buf), 0);
+		if (ret == -1)
+			exit_program_err(-1, "recv");
+		
+		/*Get real time stamp*/
+		clock_gettime(CLOCK_REALTIME,  &now);
+		timestamp = now.tv_sec * 1000000 + now.tv_nsec / 1000;
+		
+		/* Pull out the message portion and print some stats */
+		cmsg = NLMSG_DATA(buf);  //return the pointer which is pointed to msg's payload.
+		if (count % SLOW_MSG_CNT == 0)
+			printf("received %d bytes: id: %d val: %d seq: %d clen: %d\n", cmsg->len, cmsg->id.idx, cmsg->id.val, cmsg->seq, cmsg->len);
+		/* Log the data to file */
+		l = (unsigned short) cmsg->len;
+		l3 = l+4;
+		l2 = htons(l3);
+		memcpy(data,cmsg->data,1);
+		memcpy(data+1,&timestamp,8);
+		memcpy(data+9,(cmsg->data+5),l-5);
+		fwrite(&l2, 1, sizeof(unsigned short), out);
+		ret=fwrite(data,1,l3,out);
+		//fwrite(cmsg->data, 1, 1, out);
+		//fwrite(&timestamp, 1, 8, out);
+		//ret = fwrite(cmsg->data+5, 1, (l-5), out);
+		if (count % 100 == 0)
+			printf("wrote %d bytes [msgcnt=%u]\n", ret, count);
+		++count;
+		if (ret != l3)
+			exit_program_err(1, "fwrite");
+	}
+
+	exit_program(0);
+	return 0;
+}
+
+void check_usage(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <output_file>\n", argv[0]);
+		exit_program(1);
+	}
+}
+
+FILE* open_file(char* filename, char* spec)
+{
+	FILE* fp = fopen(filename, spec);
+	if (!fp)
+	{
+		perror("fopen");
+		exit_program(1);
+	}
+	return fp;
+}
+
+void caught_signal(int sig)
+{
+	fprintf(stderr, "Caught signal %d\n", sig);
+	exit_program(0);
+}
+
+void exit_program(int code)
+{
+	if (out)
+	{
+		fclose(out);
+		out = NULL;
+	}
+	if (sock_fd != -1)
+	{
+		close(sock_fd);
+		sock_fd = -1;
+	}
+	exit(code);
+}
+
+void exit_program_err(int code, char* func)
+{
+	perror(func);
+	exit_program(code);
+}
